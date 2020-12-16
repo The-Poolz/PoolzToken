ec9c62c264c719968fe1af3179ec22a7d503c73f
unknown <salty87@gmail.com>
2020-12-16 08:36:19 +0000

    Start

---
+++ contracts/POOLZ.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.2;
+import '@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
+
+contract POOLZ is ERC20Capped, ERC20Detailed{
+       constructor()
+       ERC20Detailed("Poolz.finance Token","POOLZ",18)
+       ERC20Capped(5000000*10**18)
+       public {}     
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var POOLZ = artifacts.require("POOLZ");
+
+module.exports = function(deployer) {
+  deployer.deploy(POOLZ);
+};
