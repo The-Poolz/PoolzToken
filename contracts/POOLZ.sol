a27eb637712ce7653a0c50a3f7714da8c11a8cc8
unknown <salty87@gmail.com>
2020-12-24 09:20:02 +0000

    add ERC20Burnable

--- contracts/POOLZ.sol
+++ contracts/POOLZ.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.5.2;
 import '@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
 
-contract POOLZ is ERC20Capped, ERC20Detailed{
+contract POOLZ is ERC20Capped, ERC20Detailed, ERC20Burnable  {
        constructor()
        ERC20Detailed("Poolz Finance","POOLZ",18)
        ERC20Capped(5000000*10**18)
