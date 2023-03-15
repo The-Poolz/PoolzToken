8f99203bfa6681029742f67d646f9a43d4cbaec9
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2023-03-15 16:37:31 +0200

    New Ticker

--- contracts/POOLZ.sol
+++ contracts/POOLZ.sol
@@ -5,7 +5,7 @@ import "./ERC20BurnableMintableCapped.sol";
 
 contract POOLZ is ERC20BurnableMintableCapped {
     constructor()
-        ERC20("Poolz Finance", "$POOLZ")
+        ERC20("Poolz Finance", "POOLX")
         ERC20Capped(5500000 * 10 ** 18)
     {}
 }
