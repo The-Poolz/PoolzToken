cd18023c41cee5c746298d7c65164457ccce34b6
Ashwin Arora <ashwinarora48@gmail.com>
2023-03-15 21:23:51 +0530

    name updated

--- contracts/POOLX.sol
+++ contracts/POOLX.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./ERC20BurnableMintableCapped.sol";
 
-contract POOLZ is ERC20BurnableMintableCapped {
+contract POOLX is ERC20BurnableMintableCapped {
     constructor()
         ERC20("Poolz Finance", "POOLX")
         ERC20Capped(5500000 * 10 ** 18)
--- migrations/POOLZ.js
+++
@@ -1,5 +0,0 @@
-const POOLZ = artifacts.require("POOLZ")
-
-module.exports = function(deployer) {
-  deployer.deploy(POOLZ)
-}
