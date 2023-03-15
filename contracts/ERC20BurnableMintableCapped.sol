5b1adcfe0fdc817dcfea130b65865181c93b1fd3
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-03-15 16:52:52 +0200

    added onlyMinter, refactor

--- contracts/ERC20BurnableMintableCapped.sol
+++ contracts/ERC20BurnableMintableCapped.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "./Roles.sol";
+import "./ERC20Mintable.sol";
 
-abstract contract ERC20BurnableMintableCapped is ERC20Capped, ERC20Burnable {
-    using Roles for Roles.Role;
-
+abstract contract ERC20BurnableMintableCapped is
+    ERC20Capped,
+    ERC20Mintable,
+    ERC20Burnable
+{
     function mint(address to, uint256 amount) external virtual onlyMinter {
         _mint(to, amount);
     }
@@ -17,44 +19,5 @@ abstract contract ERC20BurnableMintableCapped is ERC20Capped, ERC20Burnable {
         uint256 amount
     ) internal virtual override(ERC20Capped, ERC20) {
         super._mint(account, amount);
-    }
-
-    event MinterAdded(address indexed account);
-    event MinterRemoved(address indexed account);
-
-    Roles.Role private _minters;
-
-    constructor() {
-        _addMinter(_msgSender());
-    }
-
-    modifier onlyMinter() {
-        require(
-            isMinter(_msgSender()),
-            "MinterRole: caller does not have the Minter role"
-        );
-        _;
-    }
-
-    function isMinter(address account) public view returns (bool) {
-        return _minters.has(account);
-    }
-
-    function addMinter(address account) public onlyMinter {
-        _addMinter(account);
-    }
-
-    function renounceMinter() public {
-        _removeMinter(_msgSender());
-    }
-
-    function _addMinter(address account) internal {
-        _minters.add(account);
-        emit MinterAdded(account);
-    }
-
-    function _removeMinter(address account) internal {
-        _minters.remove(account);
-        emit MinterRemoved(account);
     }
 }
